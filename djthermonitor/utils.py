e7f9697b0bceb10224df8d903c182345838b4a7b
Dan Silvers <github@silvers.net>
2015-11-10 14:32:40 -0600

    Revert "Add value_f fields for sensor data and thresholds. Helper conversion function. Sensor temperature thresholds are in degrees fahrenheit. Data logged is in Celsius. Clarify this in the documentation and django admin."
    This reverts commit ef31d35674cb719841c1ffedf9d826f08e90b0b8.

--- djthermonitor/utils.py
+++
@@ -1,9 +0,0 @@
-from decimal import Decimal
-
-"""
-# Convert a decimal celsius temperature to fahrenheit.
-#
-# Returns an integer.
-"""
-def celsius_to_fahrenheit(temp_c_decimal)
-    return int(Decimal(9.0 / 5.0) * temp_c_decimal + Decimal(32.0))
