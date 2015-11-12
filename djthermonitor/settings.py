51663e439cdaa24b0a7bb1c13598538101d0b9ae
Dan Silvers <github@silvers.net>
2015-11-12 10:14:34 -0600

    Postgresql settings.py and provisioning scripts, DEBUG=False is now default

--- djthermonitor/settings.py
+++ djthermonitor/settings.py
@@ -10,22 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!d%)2#12%s*=v5=*(geeeofq0ci3&v5^7ju3#=&@3*9+78&ey2'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -79,16 +68,6 @@ WSGI_APPLICATION = 'djthermonitor.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
-}
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
--- djthermonitor/settings_local_default.py
+++ djthermonitor/settings_local_default.py
@@ -6,7 +6,10 @@ Rename to settings_local.py for some sane default settings.
 
 """
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEBUG = False
 
 
 """
@@ -29,3 +32,25 @@ The number of minutes we should wait until we send a notification that
 the sensor hasn't checked in for awhile.
 """
 SENSOR_DOWN_AFTER_MINUTES = 30
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djthermonitor',
+        'USER': 'djthermonitor',
+        'PASSWORD': '@@@@POSTGRESQL_PASSWORD@@@@',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
+SECRET_KEY = '@@@@DJANGO_SECRET@@@@'
